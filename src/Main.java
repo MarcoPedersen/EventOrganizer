a80e549762ad8f7048fa3380a224b81f0b910f16
Nick Hansen <nick4630@stud.kea.dk>
2018-11-22 12:02:58 +0100

    j

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello Worlfgfgbghjdeefehhfefe!");
+        System.out.println("Hello Worlfgfgbghjdeefesgsghhfefe!");
     }
 }
