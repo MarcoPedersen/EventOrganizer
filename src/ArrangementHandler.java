9b43488f7eff345ef4711578a3de6340ecdb0722
Nick Hansen <nick4630@stud.kea.dk>
2018-11-22 13:13:56 +0100

    commit

--- src/ArrangementHandler.java
+++ src/ArrangementHandler.java
@@ -6,7 +6,9 @@ public class ArrangementHandler {
 
     public static void main(String[] args) {
 
-        String connect = "INSERT INTO `arrangements`(`uid`, `name`, `type`, `info`, `yy1`, `MM1`, `dd1`, `hh1`, `mm_1`, `ss1`, `yy2`, `MM2`, `dd2`, `hh2`, `mm_2`, `ss2`) VALUES (1, 'ohyeah', '" + arrangement.type + "', '" + arrangement.info + "', 2000, 2, 28, 00, 00, 00, 2001, 2, 28, 00, 00, 00)";
+        String connect = "INSERT INTO `arrangements`(`uid`, `name`, `type`, `info`, `yy1`, `MM1`, `dd1`, `hh1`, `mm_1`, " +
+                        "`ss1`, `yy2`, `MM2`, `dd2`, `hh2`, `mm_2`, `ss2`) VALUES (1, 'ohyeah', '" + arrangement.type + "', '"
+                         + arrangement.info + "', 2000, 2, 28, 00, 00, 00, 2001, 2, 28, 00, 00, 00)";
         database.connectToDatabase(connect);
 
     }
