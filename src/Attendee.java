0a9500ac6a9f0eb89f83f8187d462306bd9f3a3c
Nick Hansen <nick4630@stud.kea.dk>
2018-11-22 12:08:53 +0100

    commit

--- src/Attendee.java
+++ src/Attendee.java
@@ -1,2 +1,12 @@
 public class Attendee {
+    private String name;
+    private String type;
+
+    public String getName() {
+        return name;
+    }
+
+    public String getType() {
+        return type;
+    }
 }
