c01bd11a7e3485c31ae5d373e675f713c2573255
Nick Hansen <nick4630@stud.kea.dk>
2018-11-22 12:55:44 +0100

    commit

--- src/Database.java
+++ src/Database.java
@@ -7,7 +7,7 @@ public class Database {
 
     String sql = "INSERT INTO `arrangements`(`uid`, `name`, `type`, `info`, `yy1`, `MM1`, `dd1`, `hh1`, `mm_1`, `ss1`, `yy2`, `MM2`, `dd2`, `hh2`, `mm_2`, `ss2`) VALUES (1, 'ohyeah', '" + arrangement.type + "', '" + arrangement.info + "', 2000, 2, 28, 00, 00, 00, 2001, 2, 28, 00, 00, 00)";
 
-    public static void makeConnection(String sql) {
+    public static void connectToDatabase(String sql) {
         String url = "jdbc:mysql://212.237.138.123:3306/thomas?useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC&useSSL=false";
 
         try {
